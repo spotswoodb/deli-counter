17f8e1392fc6be52c825e2d7ac1751eba2e0fc2c
Ben Spotswood <spotswoodb@gmail.com>
2020-11-18 12:01:19 -0600

    commit tree d7c28bfdeec2e13f13c6a60de66696aa2f57bb11

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+name = ""
+
+def line(katz_deli)
+    line_statement = []
+    if katz_deli.empty?
+        puts "The line is currently empty."
+    else
+        counter = 1
+        katz_deli.each do |customer|
+        line_statement << "#{counter}. #{customer}"
+        counter += 1
+        end
+    puts "The line is currently: #{line_statement.join(" ")}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << (name)
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{katz_deli.shift}."
+    end
+end
+    
